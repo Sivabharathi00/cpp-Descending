d164db6b8cea77258b33dad7453c455e069f385f
Sivabharathi00 <sdpssivabharathi07@gmail.com>
2026-04-30 13:04:05 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    set<int,greater<int>> siva;
+    cout<<"descending order: ";
+    for(int i=1;i<=5;i++)
+    {
+        cout<<i;
+    }
+
+    return 0;
+}
